9cfd83d6479972cbda244c9ae4a2977009a911b2
Guadalupe Canales G <lcanalesguzman@gmail.com>
2020-08-10 09:33:04 -0500

    Subir al Git hub

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import validator from './validator.js';
+const inicio= document.getElementById("inicio");
+const botonInicio= document.getElementById("botonInicio");
+const validacionDeTarjeta= document.getElementById("validacionDeTarjeta");
+const final= document.getElementById("final");
+botonInicio.addEventListener("click",solicitarDatos);
+//let numbers= document.getElementById("numbers");
+
+const botonIngresar =document.getElementById("botonIngresar");
+botonIngresar.addEventListener("click",ingresar)
+
+function solicitarDatos () {
+    //let numbersValue= numbers.value;
+    inicio.classList.add("ocultar");
+    validacionDeTarjeta.classList.remove("ocultar");
+
+   
+
+}
+
+function ingresar(){
+    //variables
+const numeroTarjeta=document.getElementById("numbers").value;
+
+    if (validator.isValid(numeroTarjeta)===true) {
+       
+        alert("correcto");
+    validacionDeTarjeta.classList.add("ocultar");
+    final.classList.remove("ocultar");
+    
+    }else{
+        alert("incorrecto");
+    }
+}
+
+
 
 console.log(validator);
+
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,49 @@
 const validator = {
   // ...
-};
+
+isValid:  function (cardNumber) {
+  let sumaimpar=0;
+let suma = 0;
+
+for (let i = 0; i < cardNumber.length; i += 1) {
+   const numeroDeTarjeta = cardNumber[i];
+  if ((i % 2) === 0) {
+    let multiplicacion = numeroDeTarjeta * 2;
+    if (multiplicacion < 9) {
+      suma = suma + multiplicacion;
+    }
+    else {
+      let convertirAString = multiplicacion.toString();
+      let sumaDeMultiplicacion = parseInt(convertirAString [0]) + parseInt(convertirAString [1]);
+      suma = suma + sumaDeMultiplicacion;
+
+    }
+    console.log("Suma Par:"+suma);
+  } 
+  else {
+    let m = numeroDeTarjeta * 1;
+   sumaimpar =sumaimpar + m;
+     console.log("Suma Impar: "+sumaimpar);
+  }
+}
+let sumatotal=sumaimpar+suma;
+
+console.log(sumatotal);
+if((sumatotal%10)===0){
+  return true;
+ 
+}else{
+  return  false;
+ 
+ }
+} ,
+maskify: function (cardNumber) {
+  let codificarNumero=cardNumber.replace(/.(?=.{4})/g, "#");
+  return codificarNumero;
+}
+
+}
+
 
 export default validator;
+
