b11bce731f3cde9e57a0d2b6c722e2de0ec6e73a
Guadalupe Canales G <lcanalesguzman@gmail.com>
2020-08-10 22:40:02 -0500

    Modificaciones

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const botonIngresar =document.getElementById("botonIngresar");
 botonIngresar.addEventListener("click",ingresar)
 
 function solicitarDatos () {
-    //let numbersValue= numbers.value;
+    
     inicio.classList.add("ocultar");
     validacionDeTarjeta.classList.remove("ocultar");
 
@@ -18,19 +18,50 @@ function solicitarDatos () {
 
 }
 
+const botonRegresar =document.getElementById("botonRegresar");
+botonRegresar.addEventListener("click",regresar)
+
+function regresar() {
+       
+    final.classList.add("ocultar");
+    validacionDeTarjeta.classList.remove("ocultar");
+}
+
+
 function ingresar(){
     //variables
-const numeroTarjeta=document.getElementById("numbers").value;
+    const numeroTarjeta=document.getElementById("numbers").value;
 
+if (validarDatos()!==false) {
     if (validator.isValid(numeroTarjeta)===true) {
        
-        alert("correcto");
+    alert("correcto");
     validacionDeTarjeta.classList.add("ocultar");
     final.classList.remove("ocultar");
     
-    }else{
-        alert("incorrecto");
     }
+    else{
+        alert("incorrecto");
+    } 
+}
+
+}
+
+
+function validarDatos() {
+    //Variable:
+    const numeroTarjeta=document.getElementById("numbers").value;
+    const resultado=document.getElementById("resultado");
+
+    if (numeroTarjeta==="") {
+        alert("Debe Ingresar un Nro de Tarjeta");
+        return false;
+    } else {
+       resultado.innerHTML="<p><b>Nro de Tarjeta: </b>"+validator.maskify(numeroTarjeta)+"</p>";
+        return true;
+    }
+
+
 }
 
 
