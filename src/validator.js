80430d9a20017117983c2b02c7f1981fd1ee8995
Guadalupe Canales G <lcanalesguzman@gmail.com>
2020-08-11 10:52:14 -0500

    Cambios luego de feedback

--- src/validator.js
+++ src/validator.js
@@ -1,46 +1,43 @@
 const validator = {
-  // ...
+  isValid:  function (cardNumber) {
+    let sumaimpar=0;
+    let suma = 0;
 
-isValid:  function (cardNumber) {
-  let sumaimpar=0;
-let suma = 0;
-
-for (let i = 0; i < cardNumber.length; i += 1) {
-   const numeroDeTarjeta = cardNumber[i];
-  if ((i % 2) === 0) {
-    let multiplicacion = numeroDeTarjeta * 2;
-    if (multiplicacion < 9) {
-      suma = suma + multiplicacion;
-    }
-    else {
+    for (let i = 0; i < cardNumber.length; i += 1) {
+      const numeroDeTarjeta = cardNumber[i];
+      if ((i % 2) === 0) {
+        let multiplicacion = numeroDeTarjeta * 2;
+        if (multiplicacion < 9) {
+          suma = suma + multiplicacion;
+        } else {
       let convertirAString = multiplicacion.toString();
-      let sumaDeMultiplicacion = parseInt(convertirAString [0]) + parseInt(convertirAString [1]);
+      let sumaDeMultiplicacion = parseInt(convertirAString[0]) + parseInt(convertirAString[1]);
       suma = suma + sumaDeMultiplicacion;
 
+        }
+    
+      } 
+      else {
+      let m = numeroDeTarjeta * 1;
+      sumaimpar =sumaimpar + m;
+     
+      } 
     }
-    console.log("Suma Par:"+suma);
-  } 
-  else {
-    let m = numeroDeTarjeta * 1;
-   sumaimpar =sumaimpar + m;
-     console.log("Suma Impar: "+sumaimpar);
-  }
-}
 let sumatotal=sumaimpar+suma;
 
-console.log(sumatotal);
-if((sumatotal%10)===0){
-  return true;
+    console.log(sumatotal);
+    if((sumatotal%10)===0){
+    return true;
  
-}else{
-  return  false;
+    }else{
+    return  false;
  
- }
-} ,
-maskify: function (cardNumber) {
+    }
+  } ,
+  maskify: function (cardNumber) {
   let codificarNumero=cardNumber.replace(/.(?=.{4})/g, "#");
   return codificarNumero;
-}
+  }
 
 }
 
